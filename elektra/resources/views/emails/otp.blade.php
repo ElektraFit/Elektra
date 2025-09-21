debc2222ea3074fecd35942f5be6325accd86f30
Eric Ghati <ericghati@gmail.com>
2025-09-21 16:26:34 +0300

    Implement 2FA with email OTP

---
+++ elektra/resources/views/emails/otp.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Your OTP Code</title>
+</head>
+<body>
+    <h2>Two-Factor Authentication</h2>
+    <p>Your OTP code is:</p>
+
+    <h3 style="color: blue; letter-spacing: 5px;">{{ $otp }}</h3>
+
+    <p>This code expires in 5 minutes</p>
+</body>
+</html>

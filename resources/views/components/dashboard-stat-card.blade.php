16a82d8ff8676e12b51f9a7e87cf5c370e922aac
Eric Ghati <ericghati@gmail.com>
2025-11-09 23:07:18 +0300

    Fix: Make icon parameter optional in DashboardStatCard component to support slots

--- resources/views/components/dashboard-stat-card.blade.php
+++ resources/views/components/dashboard-stat-card.blade.php
@@ -1,5 +1,11 @@
 <div class="stat-card {{ $instructor ? 'instructor-stat-card' : '' }}">
-    <div class="stat-icon">{!! $icon !!}</div>
+    <div class="stat-icon">
+        @if(isset($icon) && is_string($icon))
+            {!! $icon !!}
+        @else
+            {{ $slot }}
+        @endif
+    </div>
     <div class="stat-value">{{ $value }}</div>
     <div class="stat-label">{{ $label }}</div>
 </div>
